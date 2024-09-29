dc4af4a2ce3d3d2dfd20c1c1c7c5e213630af138
wenpo1975 <wenpo_1975@outlook.com>
2024-09-29 19:24:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -36,13 +36,10 @@ rm -rf ./feeds/kenzo/luci-lib-taskd
 #merge_package https://github.com/sbwml/luci-app-mosdns luci-app-mosdns
 #merge_package https://github.com/sbwml/luci-app-mosdns luci-app-mosdns/mosdns
 
-# drop mosdns and v2ray-geodata packages that come with the source
-#find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-#find ./ | grep Makefile | grep mosdns | xargs rm -f
 
-# drop mosdns and v2ray-geodata packages that come with the source
-find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-find ./ | grep Makefile | grep mosdns | xargs rm -f
+
+# remove v2ray-geodata package from feeds (openwrt-22.03 & master)
+rm -rf feeds/packages/net/v2ray-geodata
 
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
