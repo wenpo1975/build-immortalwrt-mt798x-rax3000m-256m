6a536aa1efbecd0dcbbabbd121bd21c88d7f9486
wenpo1975 <wenpo_1975@outlook.com>
2025-01-15 02:12:14 +0800

    更新 diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -34,8 +34,8 @@ g++ --version
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo "src-git kenzo https://github.com/kiddin9/kwrt-packages" >> ./feeds.conf.default
-echo "src-git small https://github.com/kenzok8/small-package" >> ./feeds.conf.default
+echo "src-git kenzo https://github.com/kiddin9/kwrt-packages" >> ./feeds.conf.default
+echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
 #echo "src-git OpenClash https://github.com/vernesong/OpenClash.git" >> ./feeds.conf.default
 
 #添加额外软件包
