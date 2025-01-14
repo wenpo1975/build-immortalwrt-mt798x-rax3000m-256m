fd130f13be265bc28b7e14a2259dc9a745e25e32
wenpo1975 <wenpo_1975@outlook.com>
2025-01-14 18:26:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -39,13 +39,13 @@ rm -rf ./feeds/kenzo/luci-app-wrtbwmon
 #rm -rf feeds/packages/lang/golang
 #git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/packages/net/v2ray-geodata
-rm -rf package/feeds/packages/mosdns
-rm -rf package/feeds/packages/v2ray-geodata
+#rm -rf feeds/packages/net/mosdns
+#rm -rf feeds/packages/net/v2ray-geodata
+#rm -rf package/feeds/packages/mosdns
+#rm -rf package/feeds/packages/v2ray-geodata
 
 
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+#git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+#git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 
