e3c7773908d74e01687e92b51fa51dd4e1b97489
wenpo1975 <wenpo_1975@outlook.com>
2025-01-15 20:40:22 +0800

    Update diy-part2.sh
    delete 
    #删除冲突的软件包
    rm -rf ./package/istore
    rm -rf ./feeds/kenzo/luci-app-quickstart
    rm -rf ./feeds/kenzo/luci-app-store
    rm -rf ./feeds/kenzo/luci-lib-taskd
    rm -rf ./feeds/kenzo/luci-app-wrtbwmon

--- diy-part2.sh
+++ diy-part2.sh
@@ -24,12 +24,7 @@ sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generat
 #sed -i 's/<0x580000 0x7200000>/<0x580000 0xee00000>/g' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-cmcc-rax3000m.dts
 #sed -i 's/116736k/240128k/g' target/linux/mediatek/image/mt7981.mk
 
-#删除冲突的软件包
-rm -rf ./package/istore
-rm -rf ./feeds/kenzo/luci-app-quickstart
-rm -rf ./feeds/kenzo/luci-app-store
-rm -rf ./feeds/kenzo/luci-lib-taskd
-rm -rf ./feeds/kenzo/luci-app-wrtbwmon
+
 
 
 # git clone https://github.com/sbwml/luci-app-openai package/openai
