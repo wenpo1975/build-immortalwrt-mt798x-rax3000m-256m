aa48711a365c62f7561363e87f969a4c303bd28c
wenpo1975 <wenpo_1975@outlook.com>
2025-01-16 00:21:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 
 # Add a feed source
 echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default
-echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
+echo "src-git small https://github.com/kenzok8/small-package" >> ./feeds.conf.default
 #echo "src-git OpenClash https://github.com/vernesong/OpenClash.git" >> ./feeds.conf.default
 #echo "src-git homeproxy https://github.com/immortalwrt/homeproxy" >> ./feeds.conf.default
 #echo "src-git fakemesh https://github.com/kenzok8/small-package/tree/main/luci-app-fakemesh" >> ./feeds.conf.default
@@ -25,12 +25,10 @@ echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
 #删除冲突的软件包homeproxy
 rm -rf ./feeds/small/luci-app-homeproxy
 rm -rf ./feeds/small/sing-box
-git clone https://github.com/immortalwrt/homeproxy kenzo/homeproxy
-git clone https://github.com/kenzok8/small-package/tree/main/luci-app-fakemesh kenzo/fakemesh
+git clone https://github.com/immortalwrt/homeproxy package/homeproxy
+git clone https://github.com/kenzok8/small-package/tree/main/luci-app-fakemesh package/fakemesh
 
-#删除冲突的软件包homeproxy
-rm -rf ./feeds/small/luci-app-homeproxy
-rm -rf ./feeds/small/sing-box
+
 
 
 #删除冲突的软件包
